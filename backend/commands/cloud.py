f1a1c11057c89d860a8e8ac7f1ad7d596fe3ced4
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-24 23:04:05 -0600

    Update cloud.py
    Command Delete

--- backend/commands/cloud.py
+++ backend/commands/cloud.py
@@ -1,15 +1,24 @@
 import boto3
 from . import config
-
+s3 = boto3.resource('s3')
+bucket_name = config.bucket_name
+bucket = s3.Bucket(config.bu)
 def create(path:str, name:str, body:str) -> str:
-  s3 = boto3.resource('s3')
   key = config.bucket_basedir + path + name
-  object = s3.Object(config.bucket_name, key)
+  object = s3.Object(bucket_name, key)
   object.put(Body=body.encode())
   return 'Archivo creado exitosamente'
 
 def delete(path, name=None) -> str:
-  return 'Falta implementar este comando'
+  for obj in bucket.objects.all():
+    objeto = config.bucket_basedir+path+name
+    if objeto == obj.key:
+      s3.Object(bucket_name, objeto).delete()
+      return 'Eliminado Exitosamente'
+    else:
+      return 'El archivo y/o Carpeta no Existe'
+
+  
 
 def modify(path:str, body:str) -> str:
   return 'Falta implementar este comando'
