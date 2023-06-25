83cc32e89c60e6bfdeb4d4a02d3e9d7580734214
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2023-06-24 23:10:41 -0600

    Update cloud.py
    Command Modify

--- backend/commands/cloud.py
+++ backend/commands/cloud.py
@@ -21,7 +21,15 @@ def delete(path, name=None) -> str:
   
 
 def modify(path:str, body:str) -> str:
-  return 'Falta implementar este comando'
+  for obj in bucket.objects.all():
+    objeto = config.bucket_basedir+path
+    if objeto == obj.key:
+      s3Object = s3.Object(bucket_name, key=(objeto))
+      s3Object.put(Body=body.encode())
+      return 'Modificado Exitosamente'
+    else:
+      return 'El archivo y/o Carpeta no Existe'
+  
 
 def renombrar(path:str, new_name:str) -> str:
     old_name = s3.object(bucket_name, path)
@@ -30,11 +38,11 @@ def renombrar(path:str, new_name:str) -> str:
        CopySource=f'{bucket_name}/{old_name}'
     )
     old_name.delete()
-    
+
 def rename(path:str, name:str) -> str:
   for obj in bucket.objects.all():
     if path == obj.key:
-        separar = path.split('/')
+        separar = config.bucket_basedir+path.split('/')
         separar.pop()
         path="/".join(separar)
         new_name = path + "/"+name
